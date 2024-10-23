8d15bd99f7b9c1b4ac2302e838c3e455f2b62651
zcemksd <zcemksd@ucl.ac.uk>
2024-10-23 21:41:45 +0100

    removed import pytest

--- test_times.py
+++ test_times.py
@@ -1,4 +1,3 @@
-import pytest
 from times import  time_range, compute_overlap_time
 
 def test_given_input():
@@ -9,4 +8,4 @@ def test_given_input():
     result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
-    
+
